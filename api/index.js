76ee31f8cf3f1133f138a8027ccbe136a78d7b0e
jackpamukci <jackpamukci824@gmail.com>
2023-12-18 20:45:11 -0500

    changed location of api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,28 @@
-import app from '../src/server'
+const express = require('express')
+const request = require('request')
 
-export default app;
+const app = express()
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+
+app.get('/api/medium', (req, res) => {
+    request(
+      { url: 'https://medium.com/feed/@readingthepitch' },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message:
+        err.message })
+      }
+      
+      res.set('Content-Type', 'application/rss+xml')
+      res.send(Buffer.from(body))
+    })
+  })
+  
+const PORT = process.env.PORT || 4050
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
+
+module.exports = app;
